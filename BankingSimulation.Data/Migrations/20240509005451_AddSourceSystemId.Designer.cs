7369d9b294a734cb8289483a58ea0b9edc4cc35b
Callum Marshall <callummarshall9@googlemail.com>
2024-05-09 01:55:52 +0100

    Add sourcesystemid column migration

--- BankingSimulation.Data/Migrations/20240509005451_AddSourceSystemId.Designer.cs
+++ BankingSimulation.Data/Migrations/20240509005451_AddSourceSystemId.Designer.cs
@@ -4,6 +4,7 @@ using BankingSimulation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BankingSimulation.Data.Migrations
 {
     [DbContext(typeof(BankSimulationContext))]
-    partial class BankSimulationContextModelSnapshot : ModelSnapshot
+    [Migration("20240509005451_AddSourceSystemId")]
+    partial class AddSourceSystemId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
