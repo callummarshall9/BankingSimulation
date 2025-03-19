d0c9382fc49fc31c2b77a990b14f8fea94797bbf
Callum Marshall <callummarsahll9@googlemail.com>
2025-03-19 17:17:52 +0000

    Fix SQLite query projection

--- BankingSimulation.Services/Processing/TransactionProcessingService.cs
+++ BankingSimulation.Services/Processing/TransactionProcessingService.cs
@@ -53,40 +53,53 @@ namespace BankingSimulation.Services.Processing
             => foundationService.GetAll<Transaction>();
 
         public IEnumerable<PeriodAccountSummary> GetCalendarEventAccountSummaries(Guid calendarId)
-                => foundationService.GetAll<Account>()
-                    .SelectMany(a => 
-                        foundationService.GetAll<CalendarEvent>()
-                        .Where(ce => ce.CalendarId == calendarId)
-                        .Select(ce => new { ce.Start, ce.End, ce.Name })
-                        .OrderBy(ce => ce.Start)
-                        .Select(ce => new PeriodAccountSummary
-                        {
-                            AccountId = a.Id,
-                            AccountName = a.Name,
-                            AccountNumber = a.Number,
-                            ComputedAccountInfo = a.Name + " (" + a.Number + ")",
-                            CalendarEventName = ce.Name,
-                            CalendarEventStart = ce.Start,
-                            CalendarEventEnd = ce.End,
-                            Incomings = Math.Round(foundationService.GetAll<Transaction>()
-                                .Where(t => 
-                                    t.Date >= ce.Start 
-                                    && t.Date <= ce.End 
-                                    && t.Value >= 0.0 
-                                    && t.AccountId == a.Id
-                                )
-                                .Sum(l => l.Value), 2),
-                            Outgoings = Math.Round(foundationService.GetAll<Transaction>()
-                                .Where(t => 
-                                    t.Date >= ce.Start 
-                                    && t.Date <= ce.End 
-                                    && t.Value <= 0.0 
-                                    && t.AccountId == a.Id)
-                                .Sum(l => l.Value), 2)
-                        }))
+        {
+            var accounts = foundationService.GetAll<Account>()
+                .ToList();
+            
+            var calendarEvents = foundationService.GetAll<CalendarEvent>()
+                .Where(ce => ce.CalendarId == calendarId)
+                .ToList();
+            
+            var results = new List<PeriodAccountSummary>();
+
+            foreach (var a in accounts)
+            {
+                foreach(var ce in calendarEvents)
+                {
+                    results.Add(new PeriodAccountSummary
+                    {
+                        AccountId = a.Id,
+                        AccountName = a.Name,
+                        AccountNumber = a.Number,
+                        ComputedAccountInfo = a.Name + " (" + a.Number + ")",
+                        CalendarEventName = ce.Name,
+                        CalendarEventStart = ce.Start,
+                        CalendarEventEnd = ce.End,
+                        Incomings = Math.Round(foundationService.GetAll<Transaction>()
+                            .Where(t => 
+                                t.Date >= ce.Start 
+                                && t.Date <= ce.End 
+                                && t.Value >= 0.0 
+                                && t.AccountId == a.Id
+                            )
+                            .Sum(l => l.Value), 2),
+                        Outgoings = Math.Round(foundationService.GetAll<Transaction>()
+                            .Where(t => 
+                                t.Date >= ce.Start 
+                                && t.Date <= ce.End 
+                                && t.Value <= 0.0 
+                                && t.AccountId == a.Id)
+                            .Sum(l => l.Value), 2)
+                    });
+                }
+            }
+
+            return results
                 .OrderBy(pas => pas.CalendarEventStart)
-                    .ThenBy(pas => pas.AccountName)
-                .ToArray();
+                .ThenBy(pas => pas.AccountName)
+                .ToList();
+        }
 
     }
 }
